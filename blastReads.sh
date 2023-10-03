9b1eb7e5342121fa990c6c13555d4a3eab6415d1
Alexander Sun <asun@scripps.edu>
2023-10-03 11:45:48 -0700

    added stranded removal to blastReads and cleaning

--- blastReads.sh
+++ blastReads.sh
@@ -2,8 +2,9 @@
 
 query=$1
 iteration=$2
+strand=$3
 
-blastn -import_search_strategy ~/nanopore/search_strategy.asn -query $query -subject ~/nanopore/modRepair.fasta -strand 'plus' \
+blastn -import_search_strategy ~/nanopore/search_strategy.asn -query $query -subject ~/nanopore/modRepair.fasta -strand $strand \
 -outfmt "6 qseqid qstart qend" 2>/dev/null | sort -k1,1 -k2,2n -u > ${iteration}_filter.txt
 
 cut -f1 ${iteration}_filter.txt | sort -u > ${iteration}_readnames.txt
--- cleaning.sh
+++ cleaning.sh
@@ -32,7 +32,9 @@ cat flipReverseND.fa >> tmp.fa
 #removes the leftmost instance of the reverse adaptor in the correct configuration
 cutadapt -a AGATCGGAAGAGCACACGTCTG -e 0.2 --overlap 10 -o noprimer.fa tmp.fa
 
-blastReads.sh noprimer.fa 1
+### blasting and removing forward reporter portions ###
+
+blastReads.sh noprimer.fa 1 'plus'
 python ~/scripts/nanopore/cutReads.py 1_reads.tsv 1_filter.txt 1_filter.fa
 
 iteration=1
@@ -40,11 +42,11 @@ iteration=1
 while :
 do
         nextIteration=$(($iteration + 1))
-        blastReads.sh ${iteration}_filter.fa $nextIteration
+        blastReads.sh ${iteration}_filter.fa $nextIteration 'plus'
         if [[ ! -s ${nextIteration}_filter.txt ]];
         then
                 #statements
-                echo "file is empty"
+                echo "no more forward reporter regions"
                 appendReads.sh ${iteration}_filter.fa ${nextIteration}_readnames.txt
                 break
         else
@@ -53,3 +55,23 @@ do
                 iteration=$(($iteration + 1))
         fi
 done
+
+### blasting and removing reverse reporter portions ###
+
+while :
+do
+        nextIteration=$(($iteration + 1))
+        blastReads.sh ${iteration}_filter.fa $nextIteration 'minus'
+        if [[ ! -s ${nextIteration}_filter.txt ]];
+        then
+                #statements
+                echo "no more reverse reporter regions"
+                appendReads.sh ${iteration}_filter.fa ${nextIteration}_readnames.txt
+                break
+        else
+                appendReads.sh ${iteration}_filter.fa ${nextIteration}_readnames.txt
+                python ~/scripts/nanopore/cutReads.py ${nextIteration}_reads.tsv ${nextIteration}_filter.txt ${nextIteration}_filter.fa
+                iteration=$(($iteration + 1))
+        fi
+done
+
