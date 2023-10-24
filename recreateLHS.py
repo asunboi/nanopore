e3e9a3e21504de8b9b7de37fdb57287e6f28f864
Alexander Sun <asun@scripps.edu>
2023-10-24 11:36:10 -0700

    changed recreateLHS.py to account for reverse reads and gaps in remaining reads

--- recreateLHS.py
+++ recreateLHS.py
@@ -41,17 +41,40 @@ def compare_and_combine(array1, array2):
 
     for a2 in array2:
         for a1 in array1:
-            if a1[0][0] <= a2[0] <= a1[0][1] and a1[0][0] <= a2[1] <= a1[0][1]:
-                result.append((a2[0]+a1[2],a2[1]+a1[2], a2[2]))
+            if a1[0][0] <= a2[0] <= a1[0][1]:
+                if a1[0][0] <= a2[1] <= a1[0][1]:
+                    result.append((a2[0]+a1[2],a2[1]+a1[2], a2[2]))
+                else:
+                    x = a2[1] - a1[0][1]
+                    result.append((a2[0]+a1[2],a1[1][1], a2[2][:-x]))
+                    array2.append((a1[1][1]+1, a2[1], a2[2][-x:]))
 
     return result
 
 file_path = "all.remaining.txt"  
 line_count = 0
+remaining_dictionaries = {}
 with open(file_path, "r") as file:
     for line in file:
+        newLine = line.replace("\n", "")
         line_count += 1
+        remaining_dictionaries[newLine] = {}
 file.close()
+
+for key, value in remaining_dictionaries.items():
+    with open(key, 'r') as file:
+        for line in file:
+            try:
+                tempArray = ast.literal_eval(line)
+                if isinstance(tempArray, list):
+                    internalKey = tempArray[0]
+                    internalValue = tempArray[1:]
+                    value[internalKey] = internalValue
+                else:
+                    print("The evaluated result is not a list.")
+            except (SyntaxError, ValueError):
+                print("Invalid input string.")
+    file.close()
 
 substringsDict = {}
 
@@ -73,33 +96,19 @@ for i in range(1,line_count+1):
                     print(f"Error message: {e}")
         file.close()
     else:
-        tempDict = {}
-        with open(f'{i-1}_filter.remaining.txt', 'r') as file:
-            for line in file:
-                try:
-                    tempArray = ast.literal_eval(line)
-                    if isinstance(tempArray, list):
-                        key = tempArray[0]
-                        values = tempArray[1:]
-                        if key in tempDict:
-                            tempDict[key].extend(values)
-                        else:
-                            tempDict[key] = values
-                    else:
-                        print("The evaluated result is not a list.")
-                except (SyntaxError, ValueError):
-                    print("Invalid input string.")
-        file.close()
         with open(f'{i}_filter.substrings.txt', 'r') as file:
             for line in file:
                 try:
                     tempArray = eval(line)
                     key = tempArray[0]
                     values = tempArray[1:]
-                    remainingArray = tempDict[key]
-                    newValues = compare_and_combine(remainingArray, values)
+                    for j in range(i-1, 0, -1):
+                        if key in remaining_dictionaries[f"{j}_filter.remaining.txt"]:
+                            remainingArray = remaining_dictionaries[f"{j}_filter.remaining.txt"][key]
+                            tempValues = compare_and_combine(remainingArray, values)
+                            values = tempValues
                     currentValues = substringsDict[key]
-                    sortedValues = sort_and_insert_tuples(newValues, currentValues)
+                    sortedValues = sort_and_insert_tuples(values, currentValues)
                     substringsDict[key] = sortedValues
                 except Exception as e:
                     print(f"Error evaluating line: {line}")
